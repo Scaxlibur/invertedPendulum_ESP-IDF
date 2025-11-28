1876f3ed73f2edd45dc2fc8591d6bcd373feb7a6
Scaxlibur <scaxlibur@gmail.com>
2025-11-28 11:08:38 +0800

    fix(motor): update GPIO pin assignments for motor control

--- main/motor.hpp
+++ main/motor.hpp
@@ -3,9 +3,9 @@
 #include "driver/ledc.h"
 #include "driver/gpio.h"
 
-#define CONTRAL_A_GPIO_NUM  GPIO_NUM_12
-#define CONTRAL_B_GPIO_NUM  GPIO_NUM_11
-#define DUTY_GPIO_NUM  GPIO_NUM_10
+#define CONTRAL_A_GPIO_NUM  GPIO_NUM_36
+#define CONTRAL_B_GPIO_NUM  GPIO_NUM_37
+#define DUTY_GPIO_NUM  GPIO_NUM_35
 
 void motor_timer_init();
 void motor_channel_init();
